14f5925a26bb4588556404ede9bb8871f3a6a62c
JuanC-27 <juang20133@gmail.com>
2023-01-22 11:37:19 -0500

    Refactor Button component to avoid repeat unnecessary code

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,12 @@
 export default function Button ({ id, name, svg, handleDelete, handleEdit }) {
   const classname = name ? 'button' : ''
 
-  if (handleDelete) {
-    return (
-      <button className={classname} onClick={() => handleDelete(id)}>
-        {name ?? <img src={svg} alt={svg} />}
-      </button>
-    )
+  const handleClick = () => {
+    if (handleDelete) return handleDelete(id)
+    if (handleEdit) return handleEdit(id)
   }
-  if (handleEdit) {
-    return (
-      <button className={classname} onClick={() => handleEdit(id)}>
-        {name ?? <img src={svg} alt={svg} />}
-      </button>
-    )
-  }
-
   return (
-    <button className={classname}>
+    <button className={classname} onClick={handleClick}>
       {name ?? <img src={svg} alt={svg} />}
     </button>
   )
